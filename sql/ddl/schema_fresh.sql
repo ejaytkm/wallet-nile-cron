7c2553d45dbf11ed3fd19083038bd4e852d74422
ejaytan <ejaytkm@gmail.com>
2025-08-15 19:54:15 +0800

    improvements - added graceful restarts & shutdowns

--- sql/ddl/schema_fresh.sql
+++ sql/ddl/schema_fresh.sql
@@ -4,21 +4,6 @@ CREATE DATABASE IF NOT EXISTS wallet_nile_cron
 
 USE wallet_nile_cron;
 
-DROP TABLE IF EXISTS queue_jobs;
-CREATE table queue_jobs
-(
-    `id`           BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    `payload`      LONGTEXT         NULL COMMENT 'Usually in JSON string format for easy parsing',
-    `attempts`     TINYINT UNSIGNED NOT NULL COMMENT 'This will be handled by coroutines',
-    `status`       ENUM ('CREATED', 'IN_QUEUE', 'PROCESSING', 'COMPLETED', 'FAILED') NOT NULL DEFAULT 'CREATED',
-    `available_at` DATETIME(3)        NULL DEFAULT NULL,
-    `completed_at` DATETIME(3)         NULL DEFAULT  NULL,
-    `cron_job_id`  INT UNSIGNED NULL DEFAULT NULL COMMENT 'Foreign key to cron_jobs.id',
-    `created_at`   DATETIME(3)             DEFAULT CURRENT_TIMESTAMP(3)
-) Engine = InnoDB collate = utf8mb4_unicode_ci;
-ALTER TABLE queue_jobs ADD INDEX idx_jobs_status (status), LOCK = NONE;
-ALTER TABLE queue_jobs ADD INDEX idx_jobs_created_at (created_at), LOCK = NONE;
-
 DROP TABLE IF EXISTS cron_jobs;
 CREATE TABLE cron_jobs
 (
@@ -33,3 +18,19 @@ CREATE TABLE cron_jobs
 ) Engine = InnoDB collate = utf8mb4_unicode_ci;
 CREATE INDEX idx_cron_jobs_status ON cron_jobs (status);
 CREATE INDEX idx_cron_jobs_merchant_id_code ON cron_jobs (merchant_id, code);
+
+DROP TABLE IF EXISTS queue_jobs;
+CREATE table queue_jobs
+(
+    `id`           BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `uuid`         VARCHAR(255)     NULL COMMENT 'Usually the id of the service provider',
+    `payload`      LONGTEXT         NULL COMMENT 'Usually in JSON string format for easy parsing',
+    `attempts`     TINYINT UNSIGNED DEFAULT 0 COMMENT 'This will be handled by coroutines',
+    `max_attempts` TINYINT UNSIGNED NOT NULL DEFAULT 1 COMMENT 'Maximum number of attempts before the job is considered failed',
+    `status`       ENUM ('CREATED', 'IN_QUEUE', 'IN_FLIGHT', 'COMPLETED', 'TIMED_OUT', 'FAILED') NOT NULL DEFAULT 'CREATED',
+    `completed_at` DATETIME(3)         NULL DEFAULT  NULL,
+    `cron_job_id` int NULL COMMENT 'This is the id of the cron job that this queue job is associated with',
+    `created_at`   DATETIME(3)             DEFAULT CURRENT_TIMESTAMP(3)
+) Engine = InnoDB collate = utf8mb4_unicode_ci;
+ALTER TABLE queue_jobs ADD INDEX idx_cron_job_id (cron_job_id), LOCK = NONE;
+ALTER TABLE queue_jobs ADD INDEX idx_created_at_status (created_at, status), LOCK = NONE;
