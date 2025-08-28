600c629fd42657d8bdb2fd438096229a7ba22727
ejaytan <ejaytkm@gmail.com>
2025-08-28 23:44:49 +0800

    test redis

--- server/health.php
+++ server/health.php
@@ -9,12 +9,17 @@ global $container;
 $merchantRp_1 = new App\Repositories\MerchantRepo('WALLET_0');
 $merchantRp_2 = new App\Repositories\MerchantRepo('WALLET_1');
 $globalRp = new App\Repositories\JobRepo();
+$redis = new App\Utils\RedisUtil();
+
+/** @var \Predis\Response\Status $status */
+$rStatus = $redis->getClient()->ping();
 
 $data = [
     "status" => "OK",
     "merchant_1" => $merchantRp_1->testConnection(),
     "merchant_2" => $merchantRp_2->testConnection(),
     "global" => $globalRp->testConnection(),
+    "redis" => $rStatus->getPayload() === 'PONG' ? 'OK' : 'FAIL',
 ];
 
 header('Content-Type: application/json');
