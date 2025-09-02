fdae6e579e7ddec42612e76ecb92f62efca3bcf5
ejaytan <ejaytkm@gmail.com>
2025-09-02 14:06:18 +0800

    update - update cron jobs

--- environment/crond/init_cron.sh
+++ environment/crond/init_cron.sh
@@ -4,10 +4,10 @@ cat >/etc/cron.d/clear_app_log <<'EOF'
 SHELL=/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 */5 * * * * www-data /usr/bin/truncate -s 0 /var/www/html/storage/logs/application.log
-*/5 * * * * root truncate -s 0 /var/log/php8.3-fpm.log
-*/5 * * * * root truncate -s 0 /var/log/nginx/access.log
-*/5 * * * * root truncate -s 0 /var/log/nginx/error.log
-*/5 * * * * root truncate -s 0 /var/log/deployment.log
+*/5 * * * * www-data truncate -s 0 /var/log/php8.3-fpm.log
+*/5 * * * * www-data truncate -s 0 /var/log/nginx/access.log
+*/5 * * * * www-data truncate -s 0 /var/log/nginx/error.log
+*/5 * * * * www-data truncate -s 0 /var/log/deployment.log
 */5 * * * root journalctl --vacuum-time=1s --unit=wallet-nile-cron
 EOF
 
