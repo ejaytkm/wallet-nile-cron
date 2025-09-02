52565a33aab2495bdd8e553923a5a93fa460cd63
ejaytan <ejaytkm@gmail.com>
2025-09-02 12:32:59 +0800

    update - update cron jobs

--- environment/crond/init_cron.sh
+++ environment/crond/init_cron.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# Create the cron job file for clearing logs
+# Create the cron job file for clearing logs = every 5 days at 6:00 AM
 cat >/etc/cron.d/clear_app_log <<'EOF'
 SHELL=/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-0 * * * * root /usr/bin/truncate -s 0 /var/www/html/storage/logs/application.log
+0 6 */5 * * root /usr/bin/truncate -s 0 /var/www/html/storage/logs/application.log
 EOF
 
 # Create the cron job file for running server/cron.php
 cat >/etc/cron.d/run_server_cron <<'EOF'
 SHELL=/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-* * * * * root /usr/bin/php /var/www/html/server/cron.php
+* * * * * www-data /usr/bin/php /var/www/html/server/cron.php
 EOF
 
 # Set the appropriate permissions and ownership for both cron jobs
